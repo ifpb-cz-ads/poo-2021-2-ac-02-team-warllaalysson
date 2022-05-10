8fd186e7b15d0ce236cb0d789295c2c887a52864
Warlla0101 <104159596+Warlla0101@users.noreply.github.com>
2022-05-10 17:14:57 -0300

    questão pratica 2 CBB

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,14 @@
+public class CBBAtividadePratica2{
+    public static void main(String[] args){
+        
+        int var1 = 100;
+        int var2 = 60;
+        int var3 = 80;
+
+        int soma = var1 + var2 + var3;
+        System.out.println("soma:"+soma);
+
+        double media = soma/3;
+        System.out.println("media:"+media);
+    }
+}
